7a82438cc591c51330b6f1ecdc58eedaf90b01b6
nbhaohao <nbhaohao@163.com>
2021-03-26 16:29:10 +0800

    feat:完成用户编辑接口

--- app/controller/user.js
+++ app/controller/user.js
@@ -81,6 +81,14 @@ class UserController extends BaseController {
       this.fail("退出登录失败");
     }
   }
+  async edit() {
+    const { ctx } = this;
+    const result = await ctx.service.user.edit({
+      ...ctx.params(),
+      updateTime: ctx.helper.time(),
+    });
+    this.success(result);
+  }
 }
 
 module.exports = UserController;
--- config/config.default.js
+++ config/config.default.js
@@ -81,7 +81,7 @@ module.exports = (appInfo) => {
       port: 6379,
       host: "127.0.0.1",
       db: 0,
-      password: "auth",
+      password: null
     },
   };
 
